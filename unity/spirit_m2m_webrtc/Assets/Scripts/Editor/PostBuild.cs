71a473d3fdc362b50ae8ff40dec34e47fa61f259
MatthiasDeFre <defrematthias@gmail.com>
2025-02-03 17:16:39 +0100

    Added copy of peer exe and config files on build

--- unity/spirit_m2m_webrtc/Assets/Scripts/Editor/PostBuild.cs
+++ unity/spirit_m2m_webrtc/Assets/Scripts/Editor/PostBuild.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
+using System.IO;
 
 public class PostBuild : IPostprocessBuildWithReport
 {
@@ -10,7 +11,19 @@ public class PostBuild : IPostprocessBuildWithReport
 
     public void OnPostprocessBuild(BuildReport report)
     {
-        Debug.Log(report.summary.outputPath);
+        string buildPath = Path.GetDirectoryName(report.summary.outputPath);
+
+        // Create directories
+        Directory.CreateDirectory(buildPath + "/spirit_unity_Data/peer");
+        Directory.CreateDirectory(buildPath + "/spirit_unity_Data/config");
+        // Copy config if not exists
+
+        // Copy peer.exe
+        File.Copy(Application.dataPath + "/peer/webRTC-peer-win.exe", buildPath + "/spirit_unity_Data/peer/webRTC-peer-win.exe", true);
+        if (!File.Exists(buildPath + "/spirit_unity_Data/config/session_config.json"))
+        {
+            File.Copy(Application.dataPath + "/config/session_config.json", buildPath + "/spirit_unity_Data/config/session_config.json", false);
+        }
         Debug.Log(Application.dataPath);
     }
 }
