de1ffb3e76d63c43ff4e3faec9bb6294b38ebff0
MatthiasDeFre <defrematthias@gmail.com>
2025-11-04 19:49:50 +0000

    Updated linux executables

--- webrtc/peer/src/sfu/sfu_connection.go
+++ webrtc/peer/src/sfu/sfu_connection.go
@@ -243,7 +243,7 @@ func (s *SFUConnection) preparePeerConnection() {
 	if err := m.RegisterHeaderExtension(webrtc.RTPHeaderExtensionCapability{URI: sdp.TransportCCURI}, webrtc.RTPCodecTypeVideo); err != nil {
 		panic(err)
 	}
-	generator, err := twcc.NewSenderInterceptor(twcc.SendInterval(10 * time.Millisecond))
+	generator, err := twcc.NewSenderInterceptor(twcc.SendInterval(1 * time.Millisecond))
 	if err != nil {
 		panic(err)
 	}
