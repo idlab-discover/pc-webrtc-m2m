7ba083b2c6805868ee57de3f7c4ad1f8e279d628
MatthiasDeFre <defrematthias@gmail.com>
2025-11-08 16:49:35 +0000

    Added iterations to bench runner

--- benchmarking/runner/bench_runner.py
+++ benchmarking/runner/bench_runner.py
@@ -7,6 +7,7 @@ n_clients = [
     [1, 0, 1, 0, 1, 0, 1, 0], 
     [1, 1, 1, 1, 1, 1, 1, 1]
 ]
+n_iterations = 2
 exp_duration = 30  # in seconds
 extra_sleep = 30
 for i in range(len(base_paths)):
@@ -15,7 +16,6 @@ for i in range(len(base_paths)):
     data = None
     with open(base_path, "r") as f:
         data = json.load(f)
-    data['logsSubDirectory'] = base_name
     data["experimentDurationSeconds"] = exp_duration
     for n_client in n_clients:
         counter = 0
@@ -23,11 +23,12 @@ for i in range(len(base_paths)):
             num = int(client["nodeID"][len("client"):])
             client["nClients"] = n_client[num-1]
             counter += n_client[num-1]
+        data['logsSubDirectory'] = f"{base_name}_cl{counter}"
         with open(f"{base_name}_{counter}.json", "w") as f:
             json.dump(data, f, indent=4)  
-            
-        subprocess.run(["python", "runner.py", "--file", 
-        f"{base_name}_{counter}.json", "--send-config", "--live"
-        ])
-        time.sleep(exp_duration + extra_sleep)
+        for _ in range(n_iterations):
+            subprocess.run(["python", "runner.py", "--file", 
+            f"{base_name}_{counter}.json", "--send-config", "--live"
+            ])
+            time.sleep(exp_duration + extra_sleep)
 
