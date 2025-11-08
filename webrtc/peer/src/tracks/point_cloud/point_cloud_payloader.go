38f375fefbca9db3d9c9c48d1785799b1b48e7c2
MatthiasDeFre <defrematthias@gmail.com>
2025-11-08 10:31:54 +0000

    Updated Linux executables

--- webrtc/peer/src/tracks/point_cloud/point_cloud_payloader.go
+++ webrtc/peer/src/tracks/point_cloud/point_cloud_payloader.go
@@ -14,7 +14,7 @@ func (p *PointCloudPayloader) Payload(mtu uint16, payload []byte) (payloads [][]
 	payloadLen := uint32(len(payload))
 	payloadRemaining := payloadLen
 	for payloadRemaining > 0 {
-		currentFragmentSize := uint32(1000)
+		currentFragmentSize := uint32(1148)
 		if payloadRemaining < currentFragmentSize {
 			currentFragmentSize = payloadRemaining
 		}
