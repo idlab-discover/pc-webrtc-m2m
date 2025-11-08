31b858b31a551664bfea40ac3294d487303350c3
MatthiasDeFre <defrematthias@gmail.com>
2025-11-08 23:03:47 +0000

    Added the ability to change session mgr config in runner

--- benchmarking/runner/bench_runner.py
+++ benchmarking/runner/bench_runner.py
@@ -2,10 +2,22 @@ import json
 import subprocess
 import time
 base_paths = ["json_example.json"]
+session_mgr_paths = [
+    [
+        "/users/madfruge/pc-webrtc-m2m-cpy/benchmarking/controller/config/manager_simple_remote_random.json",
+        "/users/madfruge/pc-webrtc-m2m-cpy/benchmarking/controller/config/manager_simple_remote_mdc.json"
+    ]
+]
+session_mgr_names = ["random", "mdc"]
+
 base_names = ["sfu1"]
 n_clients = [
     [1, 0, 1, 0, 1, 0, 1, 0], 
     [1, 1, 1, 1, 1, 1, 1, 1]
+]
+n_no_send_clients = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1]
 ]
 n_iterations = 2
 exp_duration = 30  # in seconds
@@ -17,18 +29,26 @@ for i in range(len(base_paths)):
     with open(base_path, "r") as f:
         data = json.load(f)
     data["experimentDurationSeconds"] = exp_duration
-    for n_client in n_clients:
-        counter = 0
-        for client in data["clients"]:
-            num = int(client["nodeID"][len("client"):])
-            client["nClients"] = n_client[num-1]
-            counter += n_client[num-1]
-        data['logsSubDirectory'] = f"{base_name}_cl{counter}"
-        with open(f"{base_name}_{counter}.json", "w") as f:
-            json.dump(data, f, indent=4)  
-        for _ in range(n_iterations):
-            subprocess.run(["python", "runner.py", "--file", 
-            f"{base_name}_{counter}.json", "--send-config", "--live"
-            ])
-            time.sleep(exp_duration + extra_sleep)
+    for j in range(len(session_mgr_paths[i])):
+        session_mgr_path = session_mgr_paths[i][j]
+        session_mgr_name = session_mgr_names[j]
+        data["sessionManagerConfig"]["configPath"] = session_mgr_path
+        for v in range(len(n_clients)):
+            n_client = n_clients[v]
+            counter = 0
+            for client in data["clients"]:
+                num = int(client["nodeID"][len("client"):])
+                client["nClients"] = n_client[num-1]
+                if j == 1:
+                    client["disableSendingForNClients"] = n_no_send_clients[v][num-1]
+                counter += n_client[num-1]
+            data['logsSubDirectory'] = f"{base_name}_cl{counter}"
+            with open(f"{base_name}_{session_mgr_name}_{counter}.json", "w") as f:
+                json.dump(data, f, indent=4)  
+            for k in range(n_iterations):
+                print(f"Running experiment {base_name} using {session_mgr_name} with {counter} clients, iteration {k+1}")
+                subprocess.run(["python", "runner.py", "--file", 
+                f"{base_name}_{session_mgr_name}_{counter}.json", "--send-config", "--live"
+                ])
+                time.sleep(exp_duration + extra_sleep)
 
