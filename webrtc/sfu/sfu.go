8b699e8278890480083cbc5d80b7d56c83997a59
MatthiasDeFre <defrematthias@gmail.com>
2025-11-10 15:23:24 +0000

    Update SFU

--- webrtc/sfu/sfu.go
+++ webrtc/sfu/sfu.go
@@ -440,6 +440,11 @@ func (sfu *SFU) StartPerformingQualityAdaptation() {
 					avgLoss := client.BandwidthEstimator.GetStats()["averageLoss"]
 					delayBitrate := client.BandwidthEstimator.GetStats()["delayTargetBitrate"]
 					lossBitrate := client.BandwidthEstimator.GetStats()["lossTargetBitrate"]
+					delayMeasurement := client.BandwidthEstimator.GetStats()["delayMeasurement"]
+					delayEstimate := client.BandwidthEstimator.GetStats()["delayEstimate"]
+					delayThreshold := client.BandwidthEstimator.GetStats()["delayThreshold"]
+					usage := client.BandwidthEstimator.GetStats()["usage"]
+					state := client.BandwidthEstimator.GetStats()["state"]
 					extraOutput := ""
 					if sfu.qualityAdaptation != nil {
 						extraOutput = "@activeTracks=("
@@ -449,7 +454,7 @@ func (sfu *SFU) StartPerformingQualityAdaptation() {
 						}
 						extraOutput += ")"
 					}
-					output += fmt.Sprintf("client=%d@bitrate=%d@avgLoss=%.5f@delayBitrate=%d@lossBitrate=%d%s;", client.clientID, targetBitrate, avgLoss, delayBitrate, lossBitrate, extraOutput)
+					output += fmt.Sprintf("client=%d@bitrate=%d@avgLoss=%.5f@delayBitrate=%d@lossBitrate=%d@delayMeasurement=%.2f@delayEstimate=%.2f@delayThreshold=%.2f@usage=%s@state=%s%s;", client.clientID, targetBitrate, avgLoss, delayBitrate, lossBitrate, delayMeasurement, delayEstimate, delayThreshold, usage, state, extraOutput)
 				}
 			}
 			output += "]"
